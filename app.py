639af7afff94b9da65d05c2b1f95ba8d6af0c2b9
barrakz <b.rakuzy@gmail.com>
2023-04-26 21:34:13 +0200

    Flask homepage added

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from main2 import get_job_titles
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    job_titles = get_job_titles()
+    return render_template('home.html', job_titles=job_titles)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+import json
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,38 +7,57 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-website = 'https://it.pracuj.pl/'
-path = 'C:\\chromedriver.exe'
+def get_job_titles():
+    # driver setting
+    website = 'https://it.pracuj.pl/'
+    path = 'C:\\chromedriver.exe'
 
-service = Service(path)
-chr_options = Options()
-chr_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=service, options=chr_options)
-driver.get(website)
+    service = Service(path)
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=service, options=chr_options)
+    driver.get(website)
 
-driver.maximize_window()
+    driver.maximize_window()
 
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Akceptuj wszystkie')]"))).click()
-time.sleep(2)
+    # accept cookies
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Akceptuj wszystkie')]"))).click()
+    time.sleep(1)
 
-python_filter = driver.find_element(By.XPATH, '//label[@data-test="radio-mostPopular-Python-label"]')
-python_filter.click()
-time.sleep(2)
+    # filtering by programming language
+    python_filter = driver.find_element(By.XPATH, '//label[@data-test="radio-mostPopular-Python-label"]')
+    python_filter.click()
+    time.sleep(1)
 
-remote_filter = driver.find_element(By.XPATH, '//label[@data-test="checkbox-locationsAndRemoteWork-rw-label"]')
-remote_filter.click()
-time.sleep(2)
+    # filtering by remote work
+    remote_filter = driver.find_element(By.XPATH, '//label[@data-test="checkbox-locationsAndRemoteWork-rw-label"]')
+    remote_filter.click()
+    time.sleep(1)
 
-junior_filter = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[1]/div[1]/div/div[2]/form/div[5]/div[2]/ul/li[3]/label')
-junior_filter.click()
-time.sleep(2)
+    # filtering by offers for junior
+    junior_filter = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[1]/div[1]/div/div[2]/form/div[5]/div[2]/ul/li[3]/label')
+    junior_filter.click()
+    time.sleep(1)
 
-titles = driver.find_elements(By.XPATH, '//h3[@data-test="offer-title"]')
-for title in titles:
-    print(title.text)
+    # saving the names of offers in json file
+    titles = driver.find_elements(By.XPATH, '//h3[@data-test="offer-title"]')
+    job_titles = []
 
 
-# driver.quit()
+    for title in titles:
+        job_title = {'offer_title': title.text}
+        job_titles.append(job_title)
+
+    data = {'offers': job_titles}
+
+    with open('job_titles.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+    # close browser
+    driver.quit()
+
+    return job_titles
+
 
 
 
