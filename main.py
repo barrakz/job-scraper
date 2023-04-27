04ce21e860fb543c5a906016127f64d20548463e
barrakz <b.rakuzy@gmail.com>
2023-04-27 22:05:33 +0200

    refactoring main.py file code

--- main.py
+++ main.py
@@ -7,60 +7,79 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def get_job_titles():
-    # driver setting
-    website = 'https://it.pracuj.pl/'
+def setup_driver():
     path = 'C:\\chromedriver.exe'
-
     service = Service(path)
-    chr_options = Options()
-    chr_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=service, options=chr_options)
-    driver.get(website)
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
 
-    driver.maximize_window()
-
-    # accept cookies
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Akceptuj wszystkie')]"))).click()
+def accept_cookies(driver):
+    cookie_button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Akceptuj wszystkie')]")))
+    cookie_button.click()
     time.sleep(1)
 
-    # filtering by programming language
+def filter_jobs(driver):
+    # Filtering by programming language
     python_filter = driver.find_element(By.XPATH, '//label[@data-test="radio-mostPopular-Python-label"]')
     python_filter.click()
     time.sleep(1)
 
-    # filtering by remote work
+    # Filtering by remote work
     remote_filter = driver.find_element(By.XPATH, '//label[@data-test="checkbox-locationsAndRemoteWork-rw-label"]')
     remote_filter.click()
     time.sleep(1)
 
-    # filtering by offers for junior
-    junior_filter = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div[1]/div[1]/div/div[2]/form/div[5]/div[2]/ul/li[3]/label')
+    # Filtering by offers for junior
+    junior_filter = driver.find_element(By.XPATH,
+                                         '//*[@id="root"]/div[2]/div[1]/div[1]/div/div[2]/form/div[5]/div[2]/ul/li[3]/label')
     junior_filter.click()
     time.sleep(1)
 
-    # saving the data of offers in json file
+def get_job_elements(driver):
+    # Find job offer elements
     titles = driver.find_elements(By.XPATH, '//h3[@data-test="offer-title"]')
     companies = driver.find_elements(By.XPATH, '//span[@data-test="company-name"]')
     links = driver.find_elements(By.XPATH, '//a[@data-test="offer-link"]')
     dates = driver.find_elements(By.CSS_SELECTOR, '.JobOfferstyles__FooterText-sc-1rq6ue2-22')
+    return titles, companies, links, dates
+
+def create_offers_dict(titles, companies, links, dates):
+    # Create dictionary
     offers = []
-    
-
     for title, company, link, date in zip(titles, companies, links, dates):
-        offer = {'offer_title': title.text, 'company_name': company.text, 'link': link.get_attribute('href'), 'date': date.text}
+        offer = {'offer_title': title.text, 'company_name': company.text, 'link': link.get_attribute('href'),
+                 'date': date.text}
         offers.append(offer)
+    return offers
 
+def save_to_json(offers):
+    # Save to json file
     data = {'offers': offers}
-
     with open('job_titles.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
 
-    # close chrome browser
+def get_job_titles():
+    # Set up Chrome driver
+    website = 'https://it.pracuj.pl/'
+    driver = setup_driver()
+    driver.get(website)
+    driver.maximize_window()
+
+    accept_cookies(driver)
+
+    filter_jobs(driver)
+
+    titles, companies, links, dates = get_job_elements(driver)
+
+    offers = create_offers_dict(titles, companies, links, dates)
+    
+
+    save_to_json(offers)
+
+    # Close chrome browser
     # driver.quit()
 
     return offers
-
-
-
-
